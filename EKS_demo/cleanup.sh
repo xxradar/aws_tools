e77ecaadf8bdd4c9721426d39cfe58c07a710b31
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2021-08-26 16:25:59 +0200

    Update cleanup.sh

--- EKS_demo/cleanup.sh
+++ EKS_demo/cleanup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 aws eks delete-nodegroup --cluster-name EKSdemocluster --nodegroup-name EKSdemocluster-ng
-aws eks delete-cluster --cluster-name EKSdemocluster
+aws eks delete-cluster --name EKSdemocluster
 
+
+aws iam detach-role-policy --policy-arn  arn:aws:iam::aws:policy/AmazonEKSClusterPolicy --role-name EKSdemoClusterRole
+aws iam delete-role --role-name EKSdemoClusterRole
+
+aws iam detach-role-policy --policy-arn  arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy --role-name EKSdemoNodeRole
+aws iam detach-role-policy --policy-arn  arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly --role-name EKSdemoNodeRole
+aws iam detach-role-policy --policy-arn  arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy --role-name EKSdemoNodeRole
+aws iam delete-role --role-name EKSdemoNodeRole
