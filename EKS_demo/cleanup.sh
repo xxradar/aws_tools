df8c7b6fb1663608abe1547a0b0a1eb85b4d055e
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2021-08-30 11:16:28 +0200

    Update cleanup.sh

--- EKS_demo/cleanup.sh
+++ EKS_demo/cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export AWS_REGION="eu-west-3"
-aws eks delete-nodegroup --cluster-name EKSdemocluster --nodegroup-name EKSdemocluster-ng
+aws eks delete-nodegroup --cluster-name EKSdemocluster --nodegroup-name EKSdemocluster-ng --output text
 echo "Waiting for 5min to delete NodeGroup"
 sleep 300
-aws eks delete-cluster --name EKSdemocluster
+aws eks delete-cluster --name EKSdemocluster --output text
 
 
 aws iam detach-role-policy --policy-arn  arn:aws:iam::aws:policy/AmazonEKSClusterPolicy --role-name EKSdemoClusterRole
@@ -14,4 +14,5 @@ aws iam detach-role-policy --policy-arn  arn:aws:iam::aws:policy/AmazonEC2Contai
 aws iam detach-role-policy --policy-arn  arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy --role-name EKSdemoNodeRole
 aws iam delete-role --role-name EKSdemoNodeRole
 
+echo "Delete the VPC ..."
 
