9bb886e5ef4f8bfae6e933527d582aaab253f787
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2021-12-02 10:26:41 +0100

    Create 01-creds.tf

---
+++ create_aws_users/01-creds.tf
@@ -0,0 +1,12 @@
+variable "access_key" {
+description = "The AWS access key."
+}
+
+variable "secret_key" {
+description = "The AWS access secret."
+}
+
+variable "region" {
+description = "The AWS region."
+default = "eu-west-3"
+}
